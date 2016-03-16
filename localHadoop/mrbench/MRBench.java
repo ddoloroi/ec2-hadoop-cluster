fb46ca4f65c5510a232ca5b9fe2df5109396fdcf
Mihai Varga <mihai.mv13@gmail.com>
2016-03-16 16:19:09 +0200

    mrbench works

--- localHadoop/mrbench/MRBench.java
+++ localHadoop/mrbench/MRBench.java
@@ -21,16 +21,16 @@ import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Random;
+import java.util.StringTokenizer;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.hadoop.io.UTF8;
-import org.apache.hadoop.io.WritableComparable;
 
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -54,35 +54,38 @@ public class MRBench {
      * Takes input format as text lines, runs some processing on it and
      * writes out data as text again.
      */
-    public static class Map extends MapReduceBase
-        implements Mapper<WritableComparable, Text, UTF8, UTF8> {
+    public static class Map
+        extends Mapper<Object, Text, Text, IntWritable>{
 
-        public void map(WritableComparable key, Text value,
-                OutputCollector<UTF8, UTF8> output,
-                Reporter reporter) throws IOException
-        {
-            String line = value.toString();
-            output.collect(new UTF8(process(line)), new UTF8(""));
-        }
-        public String process(String line) {
-            return line;
-        }
-    }
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
 
-    /**
-     * Ignores the key and writes values to the output.
-     */
-    public static class Reduce extends MapReduceBase
-        implements Reducer<UTF8, UTF8, UTF8, UTF8> {
-
-        public void reduce(UTF8 key, Iterator<UTF8> values,
-                OutputCollector<UTF8, UTF8> output, Reporter reporter) throws IOException
-        {
-            while(values.hasNext()) {
-                output.collect(key, new UTF8(values.next().toString()));
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(word, one);
             }
         }
     }
+
+    public static class Reduce
+        extends Reducer<Text,IntWritable,Text,IntWritable> {
+
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+            throws IOException, InterruptedException {
+
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
 
     /**
      * Generate a text file on the given filesystem with the given path name.
@@ -92,7 +95,7 @@ public class MRBench {
      * an appropriate number of leading '0' (zero) characters.    The order of
      * generated data is one of ascending, descending, or random.
      */
-    public void generateTextFile(FileSystem fs, Path inputFile,
+    public static void generateTextFile(FileSystem fs, Path inputFile,
             long numLines, Order sortOrder) throws IOException
     {
         LOG.info("creating control file: "+numLines+" numLines, "+sortOrder+" sortOrder");
@@ -142,67 +145,48 @@ public class MRBench {
     /**
      * Create the job configuration.
      */
-    private JobConf setupJob(int numMaps, int numReduces, String jarFile) {
-        JobConf jobConf = new JobConf(getConf());
-        jobConf.setJarByClass(MRBench.class);
-        FileInputFormat.addInputPath(jobConf, INPUT_DIR);
-
-        jobConf.setInputFormat(TextInputFormat.class);
-        jobConf.setOutputFormat(TextOutputFormat.class);
-
-        jobConf.setOutputValueClass(UTF8.class);
-
-        jobConf.setMapOutputKeyClass(UTF8.class);
-        jobConf.setMapOutputValueClass(UTF8.class);
-
-        if (null != jarFile) {
-            jobConf.setJar(jarFile);
-        }
-        jobConf.setMapperClass(Map.class);
-        jobConf.setReducerClass(Reduce.class);
-
-        jobConf.setNumMapTasks(numMaps);
-        jobConf.setNumReduceTasks(numReduces);
-        jobConf.setBoolean("mapreduce.job.complete.cancel.delegation.tokens", false);
-        return jobConf;
+    private static Job setupJob() throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "mrbench: word count");
+        job.setJarByClass(MRBench.class);
+        job.setMapperClass(Map.class);
+        //job.setCombinerClass(Reduce.class);
+        job.setReducerClass(Reducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, INPUT_DIR);
+        return job;
     }
 
     /**
      * Runs a MapReduce task, given number of times. The input to each run
      * is the same file.
      */
-    private ArrayList<Long> runJobInSequence(JobConf masterJobConf, int numRuns) throws IOException {
-        Random rand = new Random();
-        ArrayList<Long> execTimes = new ArrayList<Long>();
+    private static ArrayList<Job> runJobInSequence(int numRuns) throws Exception {
+        ArrayList<Job> jobs = new ArrayList<>();
 
         for (int i = 0; i < numRuns; i++) {
-            // create a new job conf every time, reusing same object does not work
-            JobConf jobConf = new JobConf(masterJobConf);
-            // reset the job jar because the copy constructor doesn't
-            jobConf.setJar(masterJobConf.getJar());
-            // give a new random name to output of the mapred tasks
-            FileOutputFormat.setOutputPath(jobConf,
-                    new Path(OUTPUT_DIR, "output_" + rand.nextInt()));
+            Job job = setupJob();
+            jobs.add(job);
+            FileOutputFormat.setOutputPath(job, new Path(OUTPUT_DIR, "output_" + i));
 
             LOG.info("Running job " + i + ":" +
-                    " input=" + FileInputFormat.getInputPaths(jobConf)[0] +
-                    " output=" + FileOutputFormat.getOutputPath(jobConf));
+                    " input=" + FileInputFormat.getInputPaths(job)[0] +
+                    " output=" + FileOutputFormat.getOutputPath(job));
 
             // run the mapred task now
-            long curTime = System.currentTimeMillis();
-            JobClient.runJob(jobConf);
-            execTimes.add(new Long(System.currentTimeMillis() - curTime));
+            job.submit();
         }
-        return execTimes;
+
+        for (int i = 0; i < numRuns; i++) {
+            while (!jobs.get(i).isComplete()) {
+                Thread.sleep(300);
+            }
+        }
+        return jobs;
     }
 
     public static void main (String[] args) throws Exception {
-        int res = ToolRunner.run(new MRBench(), args);
-        System.exit(res);
-    }
-
-    @Override
-    public int run(String[] args) throws Exception {
         String version = "MRBenchmark.0.0.2";
         System.out.println(version);
 
@@ -211,30 +195,19 @@ public class MRBench {
             "[-baseDir <base DFS path for output/input, default is /benchmarks/MRBench>] " +
             "[-jar <local path to job jar file containing Mapper and Reducer implementations, default is current jar file>] " +
             "[-numRuns <number of times to run the job, default is 1>] " +
-            "[-maps <number of maps for each run, default is 2>] " +
-            "[-reduces <number of reduces for each run, default is 1>] " +
             "[-inputLines <number of input lines to generate, default is 1>] " +
             "[-inputType <type of input to generate, one of ascending (default), descending, random>] " +
             "[-verbose]";
 
-        String jarFile = null;
         long inputLines = 1;
         int numRuns = 1;
-        int numMaps = 2;
-        int numReduces = 1;
         boolean verbose = false;
         Order inputSortOrder = Order.ASCENDING;
         for (int i = 0; i < args.length; i++) { // parse command line
-            if (args[i].equals("-jar")) {
-                jarFile = args[++i];
-            } else if (args[i].equals("-numRuns")) {
+            if (args[i].equals("-numRuns")) {
                 numRuns = Integer.parseInt(args[++i]);
             } else if (args[i].equals("-baseDir")) {
                 BASE_DIR = new Path(args[++i]);
-            } else if (args[i].equals("-maps")) {
-                numMaps = Integer.parseInt(args[++i]);
-            } else if (args[i].equals("-reduces")) {
-                numReduces = Integer.parseInt(args[++i]);
             } else if (args[i].equals("-inputLines")) {
                 inputLines = Long.parseLong(args[++i]);
             } else if (args[i].equals("-inputType")) {
@@ -257,25 +230,25 @@ public class MRBench {
         }
 
         if (numRuns < 1 ||    // verify args
-                numMaps < 1 ||
-                numReduces < 1 ||
                 inputLines < 0 ||
                 inputSortOrder == null)
             {
                 System.err.println(usage);
-                return -1;
+                System.exit(-1);
             }
 
-        JobConf jobConf = setupJob(numMaps, numReduces, jarFile);
-        FileSystem fs = FileSystem.get(jobConf);
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(conf);
         Path inputFile = new Path(INPUT_DIR, "input_" + (new Random()).nextInt() + ".txt");
         generateTextFile(fs, inputFile, inputLines, inputSortOrder);
 
         // setup test output directory
         fs.mkdirs(BASE_DIR);
-        ArrayList<Long> execTimes = new ArrayList<Long>();
+        ArrayList<Job> jobs = new ArrayList<Job>();
         try {
-            execTimes = runJobInSequence(jobConf, numRuns);
+            jobs = runJobInSequence(numRuns);
+        } catch (Exception e) {
+            System.out.println(e);
         } finally {
             // delete output -- should we really do this?
             fs.delete(BASE_DIR, true);
@@ -285,20 +258,12 @@ public class MRBench {
             // Print out a report
             System.out.println("Total MapReduce jobs executed: " + numRuns);
             System.out.println("Total lines of data per job: " + inputLines);
-            System.out.println("Maps per job: " + numMaps);
-            System.out.println("Reduces per job: " + numReduces);
         }
         int i = 0;
         long totalTime = 0;
-        for (Long time : execTimes) {
-            totalTime += time.longValue();
-            if (verbose) {
-                System.out.println("Total milliseconds for task: " + (++i) + " = " + time);
-            }
-        }
         long avgTime = totalTime / numRuns;
-        System.out.println("DataLines\tMaps\tReduces\tAvgTime (milliseconds)");
-        System.out.println(inputLines + "\t\t" + numMaps + "\t" + numReduces + "\t" + avgTime);
-        return 0;
+        System.out.println("DataLines\tAvgTime (milliseconds)");
+        System.out.println(inputLines + "\t\t" + avgTime);
+        System.exit(0);
     }
 }
