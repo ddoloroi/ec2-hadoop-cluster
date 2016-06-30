a5f4be6988c72cd2cfe27f56a9d1d2fc8fab38a5
Mihai Varga <mihai.mv13@gmail.com>
2016-06-30 14:09:19 +0100

    pearsons correlation

--- hadoop-2.7.2-src/hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/deadline/AppDeadline.java
+++ hadoop-2.7.2-src/hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/deadline/AppDeadline.java
@@ -2,11 +2,16 @@ package org.apache.hadoop.yarn.server.resourcemanager.scheduler.deadline;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.stat.correlation.Covariance;
+import org.apache.commons.math3.stat.correlation.PearsonsCorrelation;
 import org.apache.hadoop.yarn.api.records.ApplicationId;
 import org.apache.hadoop.yarn.api.records.Resource;
 import org.apache.hadoop.yarn.api.records.ResourceRequest;
 import org.apache.hadoop.yarn.server.resourcemanager.rmcontainer.RMContainer;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -22,8 +27,11 @@ public class AppDeadline implements Comparable<AppDeadline>{
     private int maxQueueLength = 3;
     private long queueSum = 0;
     private Queue<Long> lastRunTimes = new LinkedList<>();
+    private ArrayList<double []> covMatrixData = new ArrayList<>();
     private ResourceRequest resourceRequest;
     private int containersCreated = 0;
+    private double memoryFactor = 1;
+    private double vCoreFactor = 1;
 
     private ApplicationId applicationId;
 
@@ -42,13 +50,13 @@ public class AppDeadline implements Comparable<AppDeadline>{
 
     public void estimateFinishTime(ResourceRequest resourceRequest) {
         this.resourceRequest = resourceRequest;
-        int minAllocations = (resourceRequest.getCapability().getMemory() / minAllocation.getMemory()) *
-                (resourceRequest.getCapability().getVirtualCores() / minAllocation.getVirtualCores()) *
+        double minAllocations = (resourceRequest.getCapability().getMemory() / minAllocation.getMemory() * memoryFactor) *
+                (resourceRequest.getCapability().getVirtualCores() / minAllocation.getVirtualCores() * vCoreFactor) *
                 resourceRequest.getNumContainers();
         LOG.info("minAllocations memory=" + resourceRequest.getCapability().getMemory() +
             " vCores=" + resourceRequest.getCapability().getVirtualCores() +
             " containers=" + resourceRequest.getNumContainers());
-        estimatedFinishTime = System.currentTimeMillis() / 1000 + avgTimePerMinAllocation * minAllocations;
+        estimatedFinishTime = System.currentTimeMillis() / 1000 + (int)(avgTimePerMinAllocation * minAllocations);
         slack = deadline - estimatedFinishTime;
         LOG.info("Estimated finish time = " + new Date(estimatedFinishTime * 1000) +
             " deadline = " + new Date(deadline * 1000));
@@ -61,8 +69,20 @@ public class AppDeadline implements Comparable<AppDeadline>{
     synchronized public void addCompletedContainer(RMContainer rmContainer) {
         long runTime = (rmContainer.getFinishTime() - rmContainer.getCreationTime()) / 1000;
         Resource usedResource = rmContainer.getAllocatedResource();
-        int factor = (usedResource.getMemory() / minAllocation.getMemory()) *
-                (usedResource.getVirtualCores() / minAllocation.getVirtualCores());
+        int vCores = usedResource.getVirtualCores() / minAllocation.getVirtualCores();
+        int memory = usedResource.getMemory() / minAllocation.getMemory();
+        covMatrixData.add(new double[] {vCores, memory, runTime});
+        if (covMatrixData.size() > maxQueueLength) {
+            covMatrixData.remove(0);
+        }
+
+        if (covMatrixData.size() > 1) {
+            RealMatrix cor = new PearsonsCorrelation(covMatrixData.toArray(new double[0][0])).getCorrelationMatrix();
+            vCoreFactor = Math.abs(Double.isNaN(cor.getEntry(0, 2)) ? 1.0 : cor.getEntry(0, 2));
+            memoryFactor = Math.abs(Double.isNaN(cor.getEntry(1, 2)) ? 1.0 : cor.getEntry(1, 2));
+        }
+
+        double factor = (vCores * vCores) * (memory * memoryFactor);
         runTime *= factor;
         queueSum += runTime;
         lastRunTimes.add(runTime);
